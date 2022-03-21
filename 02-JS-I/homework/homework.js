64c5fc4b8b91a293c41ad58c9364d2d6a7faac04
LuCardozo <lucianocar12300@gmail.com>
2022-03-21 14:19:47 -0300

    resueltos

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "nashe";
+const nuevaString = "ola";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 2;
@@ -264,7 +264,7 @@ function esVocal(letra){
     }
   }
     else {
-      return "Dato incorrecto";
+      return "Dato incorreco";
     }
   
 }
